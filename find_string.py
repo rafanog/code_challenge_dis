afc714f29e421dbd53ca2ef0f2064e3e2646c288
Rafael Noguera <rafanog2155@gmail.com>
2021-04-13 22:35:53 -0300

    final corrections before first final version

--- find_string.py
+++ find_string.py
@@ -1,5 +1,5 @@
 """
-ejecutar la aplicación de consola"FINDSTR" y buscar sobre un archivo "test.txt"
+Ejecutar la aplicación de consola"FINDSTR" y buscar sobre un archivo "test.txt"
 que tenga como contenido "esto es un archivo de ejemplo" estos dos strings:
 "esto es" y "esto no es".
 El resultado deberia ser algo como:
@@ -16,15 +16,35 @@ def retFileContent(text, filepath): #This executes the findstr shell command
 ################################################################################
 def testEstoEs(text, filepath): # This test case verifies that "esto es" is present in a text file
     result = "PASS" if retFileContent(text, filepath) == 0 else "FAILED"
-    print(result)
+    print("Test 1: \n\"esto es\"        - esta presente en el archivo. {}".format(result))
     return result
 
 def testEstoNoEs(text, filepath): #This test cases verifies that "esto no es" isn't present in a text file
     result = "PASS" if retFileContent(text, filepath) == 1 else "FAILED"
-    print(result)
+    print("Test 1: \n\"esto no es\"        - no esta presente en el archivo. {}".format(result))
     return result
 ################################################################################
 def suiteEsto():
     tcList = []
+    tcPassed = 0
+    tcFailed = 0
+    counter = 0
+
     tcList.append(testEstoEs("esto es", "test.txt"))
-    tc_list.append(testEstoNoEs("esto no es", "test.txt"))
+    tcList.append(testEstoNoEs("esto no es", "test.txt"))
+
+    for t in tcList:
+        if tcList[counter] == "PASS":
+            tcPassed += 1
+        else:
+            tcFailed += 1
+
+        counter += 1
+
+    if tcFailed > 0:
+        print("Failed Tests {}/{} ERROR.".format(tcFailed, len(tcList)))
+
+    print("Success Tests {}/{} OK".format(tcPassed, len(tcList)))
+################################################################################
+
+suiteEsto()
