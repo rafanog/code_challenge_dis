3d80f898bfd2a8df003678ca3b41205c6e17d8e8
Rafael Noguera <rafanog2155@gmail.com>
2021-04-14 16:17:44 -0300

    Finalizing first final version
    initial tests added

--- verify_notepad.py
+++ verify_notepad.py
@@ -1,4 +1,5 @@
 import subprocess as sp
+import time
 import re
 import os
 """
@@ -6,40 +7,58 @@ Ejecutar Notepad.exe, esperar 10 segundos y luego si la app no fue cerrada por u
 Dependiendo de si fue cerrada manualmente o terminada por el script debería imprimir dos resultados distintos.
 EJ:
 App: notepad.exe, pid: 1234, "Cerrada por el usuario"
-App: notepad.exe, pid: 1234, "Terminada por tiemeout"
+App: notepad.exe, pid: 1234, "Terminada por timeout"
 """
 def execProcess(processName):
     #os.system("{}.exe".format(processName))
-    sp.Popen(processName)
+    p = sp.Popen([processName])
+
+def termProcess(processName):
+    os.system("TASKKILL /F /IM {} > nul".format(processName))
 
 def getTaskList():
-    tasks = sp.check_output(['tasklist']).decode('cp866', 'ignore').split("\r\n") #.split("\r\n")
+    tasks = sp.check_output(['tasklist']).decode('cp866', 'ignore').split("\r\n")
 
-    p = []
+    p = {}
     for task in tasks:
         m = re.match("(.+?) +(\d+) (.+?) +(\d+) +(\d+.* K).*",task)
         if m is not None:
-            p.append(m.group(1))
+            #p.append({"process":m.group(1).upper(), "pid":m.group(2)})
+            p.update([(m.group(1).upper(), m.group(2))])
             """
-            #this creates a more detailed list but commenting it out since
-            #current requirement only needs the process name
-            ,
-            "pid":m.group(2),
+            #this creates a more detailed tasklist but commenting it out since
+            #current requirement only needs the process name and pid
             "session_name":m.group(3),
             "session_num":m.group(4),
             "mem_usage":m.group(5)
             """
-
+    ################################## prints process list
+    """
+    for process in p:
+        print("process: {} - PID: {}".format(process, p[process]))
+    """
+    ##################################
     return p
 
 def executeAndInspect(processName):
     print("Opening {}".format(processName))
-    execProcess(processName)
+    execProcess(processName) #open notepad and verify it correctly opened
     p = getTaskList()
-    if processName not in p:
+    if processName.upper() not in p:
         print("Error while trying to open {}".format(processName))
     else:
-        print("{} process opened!")
+        print("{} process opened!".format(processName))
 
+    time.sleep(10) #wait for 10 secs
+    result = "App - {}, pid: {}, ".format(processName, p[processName.upper()])
+    p = getTaskList() #retrieve tasklist
+    if processName.upper() not in p: #verify if it was closed by the user
+        reason = "Cerrada por el usuario"
+    else:
+        termProcess(processName) #forcefully close
+        reason = "Terminada por timeout"
+    result += " \"{}\"".format(reason)
+
+    print(result)
 
 executeAndInspect("notepad.exe")
